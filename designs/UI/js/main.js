43fc2ff2cee379949a644ed2579422febabdc348
Justin Ndwiga <ndwigajustin@gmail.com>
2018-02-27 16:28:09 +0300

    > [finishes #155551320] Dashboard design

---
+++ designs/UI/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$('.nav-trigger').click(function() {
+		$('.side-nav').toggleClass('visible');
+		// $('.main').css("margin-top", "290px");
+		// $('.title').css("margin-top", "290px");
+	});
+});
+
+// $(document).ready(function() {
+// ('nav').hide();
+// ('.nav-trigger').click(function(){
+// 	('nav').slideToggle("slow", function(){
+// 		('nav-trigger').hide()
+// 	});
+// });
+
+// });
