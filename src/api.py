f555e442799b74ab614fdf626f47f36676250448
Justin Ndwiga <ndwigajustin@gmail.com>
2018-03-05 23:06:10 +0300

    [finishes #155529238] add endpoints to login,logout, reset-password and get all users, add token_required decorator

--- src/api.py
+++ src/api.py
@@ -1,29 +1,104 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response,session
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
+from passlib.hash import sha256_crypt
+import jwt
+import datetime
+from functools import wraps
+
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'doordonotthereisnotry'
 
+
 users = [
 ]
+def find_user_by_username(username):
+    for user in users:
+        if user['username'] == username:
+            return user
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return jsonify({'Message' : 'Token is missing!'}), 401
+
+        try: 
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = find_user_by_username(data['username'])
+        except:
+            return jsonify({'Message' : 'Token is invalid!'}), 401
+
+        return f(current_user, *args, **kwargs)
+
+    return decorated
 
 
 @app .route('/api/auth/register',  methods=['POST'])
 def create_user():
-    user = {'username':  request.json['username'], 'password':
-            request.json['password'], 'id': len(users)+1}
-    for u in users:
-        if request.json['username'] in u.values():
+    new_user = {'id': str(uuid.uuid4()), 'username':  request.json['username'],
+                'password': sha256_crypt.encrypt(str(request.json['password'])), "admin": False}
+    for user in users:
+        if new_user['username'] == user['username']:
             return jsonify({'Message': "User already exists"}), 400
         if request.json['username'] == "" or request.json['password'] == "":
             return jsonify({'Message': 
-            "Username and Password is required"}),400
+                "Username and Password is required"}),400
 
-    users.append(user)
-    return jsonify({'Message': 'User registered successfully'}), 201
+    users.append(new_user)
+    return jsonify({"Message": "User registered successfully"}), 201
+
+
+@app.route('/api/auth/login', methods = ['POST'])
+def login():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return jsonify({"Message": "Auth missing"}), 401
+    
+    for user in users:
+        if auth.username == user['username'] and auth.password == user['password'] :
+            token = jwt.encode({'username': auth.username,  'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=20)}, app.config['SECRET_KEY'])
+        
+            return jsonify({"token": token.decode("UTF-8")}), 200# for python3 have to decode to reg string since it's generated in bytes
+
+    return jsonify({"Message": "Invalid login" }),401
+
+
+@app.route('/api/auth/logout', methods = ['DELETE'])
+@token_required
+def logout(current_user):
+    session.clear()
+    return jsonify({"Message": "Logged out"}), 201
+
+
+@app.route('/api/auth/reset-password', methods=['PUT'])
+@token_required
+def reset_password(current_user):
+    """
+    User must be logged in to update password
+    """
+    data = request.get_json()
+    response = find_user_by_username(current_user['username'])
+    if data['password']:
+        response['password'] = sha256_crypt.encrypt(str(data['password']))
+        return jsonify({'msg': 'password updated'}), 200
+    return jsonify({'warning': 'password cannot be empty'}), 403
+
+
+@app.route('/api/auth/users', methods=['GET'])
+def get_all_users():
+    return jsonify({"users": users}), 200
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
+ 
