11d980f28e339a57110a44707422ec3a2f8fe0d4
Justin Ndwiga <ndwigajustin@gmail.com>
2018-04-01 01:40:44 +0300

    [chore] increase test coverage

--- tests/v2/test_user.py
+++ tests/v2/test_user.py
@@ -49,7 +49,7 @@ class UserAuthClass(TestSetUp):
         response_msg = json.loads(response.data.decode("UTF-8"))
         self.assertIn("spaces", response_msg["Message"])
 
-    def test_username_has_enouh_characters(self):
+    def test_username_has_enough_characters(self):
         """Tests error raised when username has less then 3 characters."""
         response = self.app.post("/api/v2/auth/register",
                                  data=json.dumps(dict(username="am",
@@ -64,6 +64,16 @@ class UserAuthClass(TestSetUp):
         response = self.app.post("/api/v2/auth/register",
                                  data=json.dumps(dict(username="testusername",
                                                       password="", first_name="first", last_name="last")),
+                                 content_type="application/json")
+        self.assertEqual(response.status_code, 400)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertIn("required", response_msg["Message"])
+
+    def test_missing_first_or_last_name(self):
+        """Tests error raised when first name or last name is missing."""
+        response = self.app.post("/api/v2/auth/register",
+                                 data=json.dumps(dict(username="testusername",
+                                                      password="testpassword", first_name="", last_name="last")),
                                  content_type="application/json")
         self.assertEqual(response.status_code, 400)
         response_msg = json.loads(response.data.decode("UTF-8"))
@@ -99,6 +109,15 @@ class UserAuthClass(TestSetUp):
         self.assertEqual(response.status_code, 200)
         response_msg = json.loads(response.data.decode("UTF-8"))
         self.assertIn("token", response_msg)
+    
+    def test_missing_credentials(self):
+        """Tests error raised for missing auth details."""
+        response = self.app.post("/api/v2/auth/login",
+                                 data=json.dumps(dict(username="", password="")),
+                                 content_type="application/json")
+        self.assertEqual(response.status_code, 401)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertIn("required", response_msg["Message"])
 
     def test_invalid_username_login(self):
         """Tests unauthorized error raised with invalid username."""
@@ -176,16 +195,41 @@ class UserAuthClass(TestSetUp):
         response_msg = json.loads(response.data.decode("UTF-8"))
         self.assertIn("updated", response_msg["Message"])
 
+    def test_get_one_user(self):
+        """Tests authorized user can get a specific user profile"""
+        response = self.app.get('/api/v2/auth/users/{}'.format(User.query.order_by(User.created_at).first().id),
+                                content_type="application/json",
+                                headers={"x-access-token": self.admintoken})
+        self.assertEqual(response.status_code, 200)
+        
     def test_get_all_users(self):
         """Test if get method gets all registered users"""
         response = self.app.get('/api/v2/auth/users',
                                 content_type="application/json",
                                 headers={"x-access-token": self.admintoken})
         self.assertEqual(response.status_code, 200)
+    
+    def test_get_all_users_unauthorized(self):
+        """Tests error raised for accessing user list for non-admins"""
+        response = self.app.get('/api/v2/auth/users',
+                                content_type="application/json",
+                                headers={"x-access-token": self.token})
+        self.assertEqual(response.status_code, 401)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertIn("Cannot perform", response_msg["Message"])
+
+    def test_get_one_user_unauthorized(self):
+        """Tests error raised for accessing a user for non-admin"""
+        response = self.app.get('/api/v2/auth/users/{}'.format(User.query.order_by(User.created_at).first().id),
+                                content_type="application/json",
+                                headers={"x-access-token": self.unkowntoken})
+        self.assertEqual(response.status_code, 401)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertIn("Cannot perform", response_msg["Message"])
 
     def test_get_user_404(self):
         """Test error raised when accessing nonexisting user"""
-        response = self.app.get('/api/v2/auth/users/3000',
+        response = self.app.get('/api/v2/auth/users/00',
                                 content_type="application/json",
                                 headers={"x-access-token": self.admintoken})
         self.assertEqual(response.status_code, 404)
@@ -204,6 +248,19 @@ class UserAuthClass(TestSetUp):
         self.assertEqual(response.status_code, 200)
         response_msg = json.loads(response.data.decode("UTF-8"))
         self.assertIn("admin", response_msg["Message"])
+    
+    def test_unauthorized_promote_user(self):
+        """Test error raised for unauthorized user promotion"""
+        response = self.app.put(
+            '/api/v2/auth/users/{}'.format(
+                User.query.order_by(
+                    User.created_at).first().id),
+            content_type="application/json",
+            headers={
+                "x-access-token": self.token})
+        self.assertEqual(response.status_code, 401)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertIn("Cannot perform", response_msg["Message"])
 
     def test_delete_user(self):
         """Tests deleting user from db"""
@@ -217,6 +274,25 @@ class UserAuthClass(TestSetUp):
         self.assertEqual(response.status_code, 200)
         response_msg = json.loads(response.data.decode("UTF-8"))
         self.assertIn("deleted", response_msg["Message"])
+
+    def test_unauthorized_user_delete(self):
+        """Tests error raised for unauthorized user delete from db"""
+        response = self.app.delete(
+            '/api/v2/auth/users/{}'.format(
+                User.query.order_by(
+                    User.created_at).first().id),
+            content_type="application/json",
+            headers={
+                "x-access-token": self.token})
+        self.assertEqual(response.status_code, 401)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertIn("Cannot perform", response_msg["Message"])
+
+    def test_read_user_business(self):
+        """Tests user can read the businesses of a particular user"""
+        response = self.app.get('/api/v2/auth/users/{}/businesses'.format(User.query.order_by(User.created_at).first().id),
+                                content_type="application/json")
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
