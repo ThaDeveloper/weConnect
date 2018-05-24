2930a64294fa848728cddbc357b83d486551e86c
Justin Ndwiga <ndwigajustin@gmail.com>
2018-05-24 10:24:34 +0300

    Remove user auth for getting reviews

--- src/v2/views.py
+++ src/v2/views.py
@@ -152,7 +152,7 @@ def create_review(current_user, id):
 
 
 @biz.route('/businesses/<int:id>/reviews', methods=['GET'])
-def get_business_reviews(current_user, id):
+def get_business_reviews(id):
     """List all business' reviews"""
     business = Business.query.filter_by(id=id).first()
     if not business:
